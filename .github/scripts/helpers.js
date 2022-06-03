08313bbeb69c2840b504fa8c279458a7de322f12
kiyan <yazdani.kiyan@protonmail.com>
2022-06-03 09:50:50 +0200

    Revert "ci(smart): update to v2 (#74)"
    This reverts commit 35c858e9ef6a82920820645c0ce498e78cd4ca2a.

--- .github/scripts/helpers.js
+++ .github/scripts/helpers.js
@@ -1,3 +1,5 @@
+const packageJson = require('../../package.json');
+
 const Reactions = {
   Confused: 'confused',
   Eyes: 'eyes',
@@ -9,7 +11,18 @@ const Reactions = {
   '-1': '-1',
 };
 
-const commentSignature = '';
+const Gifs = {
+  NotFound: '![Not found](https://media.giphy.com/media/14uQ3cOFteDaU/giphy-downsized.gif)',
+  Deploying: '![Deploying](https://media.giphy.com/media/dVnzGW7UehcEpwLxBm/giphy.gif)',
+  Deployed: '![Deployed](https://media.giphy.com/media/tOxRB2tvzTscQPwXHv/giphy.gif)',
+};
+
+const commentSignature = '\n\n> **Masteos Deploy Bot** - Type ``/help`` to see available commands';
+const codepushApp = {
+  ios: 'masteos-tech/masteos',
+  android: 'masteos-tech/masteos-android',
+};
+
 const commentHistory = [];
 
 const useHelpers = ({ args: commandArgs, github, context, exec: githubExec }) => {
@@ -75,6 +88,16 @@ const useHelpers = ({ args: commandArgs, github, context, exec: githubExec }) =>
     return pullRequest.data;
   };
 
+  const findApiPullRequests = async () => {
+    const pullRequests = await github.rest.pulls.list({
+      ...context.repo,
+      repo: 'masteos',
+      per_page: 100,
+    });
+
+    return pullRequests.data;
+  };
+
   const addComment = body =>
     github.rest.issues
       .createComment({
@@ -109,19 +132,173 @@ const useHelpers = ({ args: commandArgs, github, context, exec: githubExec }) =>
       content,
     });
 
+  const getPermissionLevel = async () => {
+    const actorAccess = await github.rest.repos.getCollaboratorPermissionLevel({
+      ...context.repo,
+      username: context.actor,
+    });
+
+    return actorAccess.data.permission;
+  };
+
+  const getRelatedApiPullRequest = async apiBranchName => {
+    const apiPullRequests = await findApiPullRequests();
+    let targetName = apiBranchName ?? (await getCurrentPullRequest()).head.ref;
+    return { targetName, targetPr: apiPullRequests.find(pr => pr.head.ref === targetName) };
+  };
+
+  const getLatestNativeBuildVersion = () => packageJson.version;
+  const getPreviousNativeBuildVersion = async () => {
+    const lastRelease = await github.rest.repos.getLatestRelease({
+      owner,
+      repo,
+    });
+    return lastRelease.tag_name;
+  };
+
   return {
     exec,
     parseCommand: text => text.match(/^\/([\S]+) *(.*)?$/m),
     addComment,
     smartComment,
     addReaction,
+    getPermissionLevel,
     getCurrentPullRequest,
     getPullRequestNumber,
+    getRelatedApiPullRequest,
+    findApiPullRequests,
     firstArgument: commandArgs?.trim().split(' ')[0],
+    heroku: {
+      getAppInfo: app => exec('heroku', ['apps:info', app, '--json']),
+      setAppApiUrl: (app, apiUrl) => exec('heroku', ['config:set', `API_URL=${apiUrl}`, '-a', app]),
+    },
+    git: {
+      getLatestNativeBuildVersion,
+      getPreviousNativeBuildVersion,
+    },
+    appcenter: {
+      listCodepushDeployments: () =>
+        exec('yarn', [
+          'appcenter',
+          'codepush',
+          'deployment',
+          'list',
+          '-ka',
+          'masteos-tech/masteos-android',
+        ]),
+      createDeployment: (name, opts = { android: true, ios: true }) => {
+        const commands = [];
+
+        const computeBaseArguments = target => [
+          'appcenter',
+          'codepush',
+          'deployment',
+          'add',
+          '-a',
+          codepushApp[target],
+          name,
+        ];
+
+        if (opts.ios) {
+          commands.push(exec('yarn', computeBaseArguments('ios')));
+        }
+
+        if (opts.android) {
+          commands.push(exec('yarn', computeBaseArguments('android')));
+        }
+
+        return Promise.all(commands);
+      },
+      releaseDeployment: async (name, opts = { android: true, ios: true }) => {
+        const commands = [];
+
+        const latestBinaryRelease = getLatestNativeBuildVersion();
+
+        const computeBaseArguments = target => [
+          'appcenter',
+          'codepush',
+          'release-react',
+          '--extra-bundler-option=--reset-cache',
+          '--mandatory',
+          '--disable-duplicate-release-error',
+          '--app',
+          codepushApp[target],
+          '-d',
+          name,
+          '-t',
+          latestBinaryRelease,
+          '--token',
+          process.env.APPCENTER_ACCESS_TOKEN,
+        ];
+
+        const env = {
+          NPM_TOKEN: process.env.NPM_TOKEN,
+        };
+
+        switch (name.toLowerCase()) {
+          case 'production':
+            env.ENVIRONMENT = 'production';
+            env.API_URL = 'https://masteosv2.herokuapp.com';
+            break;
+          case 'staging':
+            env.ENVIRONMENT = 'staging';
+            env.API_URL = 'https://masteos-api-staging.herokuapp.com';
+            break;
+          default:
+            env.ENVIRONMENT = 'development';
+            env.API_URL = opts.apiUrl ?? 'https://masteos-api-staging.herokuapp.com';
+            break;
+        }
+
+        const results = {
+          ios: null,
+          android: null,
+        };
+
+        if (opts.ios) {
+          results.ios = commands.push(
+            await exec('yarn', computeBaseArguments('ios'), { env, silent: false })
+          );
+        }
+
+        if (opts.android) {
+          results.android = commands.push(
+            await exec('yarn', computeBaseArguments('android'), { env, silent: false })
+          );
+        }
+
+        return results;
+      },
+      removeDeployment: async (name, opts = { android: true, ios: true }) => {
+        const commands = [];
+
+        const computeBaseArguments = target => [
+          'appcenter',
+          'codepush',
+          'deployment',
+          'remove',
+          '-a',
+          codepushApp[target],
+          '--quiet',
+          name,
+        ];
+
+        if (opts.ios) {
+          commands.push(exec('yarn', computeBaseArguments('ios')));
+        }
+
+        if (opts.android) {
+          commands.push(exec('yarn', computeBaseArguments('android')));
+        }
+
+        return Promise.all(commands);
+      },
+    },
   };
 };
 
 module.exports = {
   Reactions,
+  Gifs,
   useHelpers,
 };
