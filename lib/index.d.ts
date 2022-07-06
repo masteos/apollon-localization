338181984ecc3f2749300f8f51949136500b3cd0
jm-masteos <jean-marc.cheynier@masteos.com>
2022-07-06 16:44:54 +0200

    feat(explo-translation-namespaces)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,7 +4,3 @@ export { default as frBE } from '../langs/fr_BE.json';
 export { default as frFR } from '../langs/fr_FR.json';
 export { default as nl } from '../langs/nl.json';
 export { default as nlBE } from '../langs/nl_BE.json';
-export { default as realEstateBEen_US } from '../langs/realEstateBE_en_US.json';
-export { default as realEstateBEfr_FR } from '../langs/realEstateBE_fr_FR.json';
-export { default as realEstateFRen_US } from '../langs/realEstateFR_en_US.json';
-export { default as realEstateFRfr_FR } from '../langs/realEstateFR_fr_FR.json';
